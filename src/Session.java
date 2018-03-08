603ae60060e05b7fac43b8ddec10e711019405cd
Omair Ijaz <oijaz@iastate.edu>
2018-03-08 08:56:44 -0600

    Fix unit overflow in Session toString

--- src/Session.java
+++ src/Session.java
@@ -96,9 +96,10 @@ public class Session {
 		Date dateStart = new Date(this.timePairList.get(0).getStartTime());
 	
 		// get duration and convert to seconds
-		int durationSeconds = (int)this.getTotalTime() / 1000;
-		int durationMinutes = durationSeconds / 60;
-		int durationHours = durationMinutes / 60;
+		int seconds = (int) this.getTotalTime() / 1000;
+		int durationSeconds = seconds % 60;
+		int durationMinutes = (seconds / 60) % 60;
+		int durationHours = (seconds / 60) / 60;
 		
 		String durationString = String.format("%d:%02d:%02d", durationHours, durationMinutes, durationSeconds);
 		
