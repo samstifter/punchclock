05dca7beef86cd47d5eaaab936872712a41a9ed1
Omair Ijaz <oijaz@iastate.edu>
2018-03-08 10:21:01 -0600

    Fix duration method for value wrap

--- src/Session.java
+++ src/Session.java
@@ -5,117 +5,119 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * This class defines a Session.
- * A session holds pairs of time pairs.
+ * This class defines a Session. A session holds pairs of time pairs.
  * 
  */
 public class Session {
 	private String sessionName;
 	private List<TimePair> timePairList;
-	
+
 	/**
 	 * A session object holding a list of time pairs.
 	 */
-	public Session () {
+	public Session() {
 		this.timePairList = new ArrayList<TimePair>();
 
 	}
-	
+
 	/**
 	 * A session object holding a list of time pairs with a given name.
-	 * @param name 
-	 * 		The name given to the session
+	 * 
+	 * @param name
+	 *            The name given to the session
 	 */
 	public Session(String name) {
 		this.sessionName = name;
 		this.timePairList = new ArrayList<TimePair>();
 	}
-	
+
 	/**
 	 * Returns the name of the session.
+	 * 
 	 * @return Session name
 	 */
 	public String getSessionName() {
 		return this.sessionName;
 	}
-	
+
 	/**
 	 * Sets the name of the session.
+	 * 
 	 * @param sessionName
-	 * 		name to set session
+	 *            name to set session
 	 */
 	public void setSessionName(String sessionName) {
 		this.sessionName = sessionName;
 	}
-	
+
 	/**
 	 * Returns the sessions list of time pairs.
-	 * @return
-	 * 		timePair
+	 * 
+	 * @return timePair
 	 */
 	public List<TimePair> getTimePairList() {
 		return timePairList;
 	}
-	
+
 	/**
-	 * Sets the sessions time pair list. 
+	 * Sets the sessions time pair list.
+	 * 
 	 * @param timePairList
 	 */
 	public void setTimePairList(List<TimePair> timePairList) {
 		this.timePairList = timePairList;
 	}
-	
+
 	/**
 	 * 
 	 * @param tp
 	 */
 	public void addTimePair(TimePair tp) {
-    	if(tp != null)
-    		timePairList.add(tp);
-    }
+		if (tp != null)
+			timePairList.add(tp);
+	}
 
 	/**
 	 * Gets the total elapsed time of all time pairs in a session.
-	 * @return
-	 * 		Total elapsed time in session.
+	 * 
+	 * @return Total elapsed time in session.
 	 */
 	public long getTotalTime() {
 		long time = 0;
-		for(TimePair tp : timePairList) {
+		for (TimePair tp : timePairList) {
 			time += tp.getElapsedTime();
 		}
 		return time;
 	}
-	
+
 	public String toString() {
-		if(this.getTimePairList().isEmpty()){
+		if (this.getTimePairList().isEmpty()) {
 			return "Empty session";
 		}
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date dateStart = new Date(this.timePairList.get(0).getStartTime());
-	
+
 		// get duration and convert to seconds
 		int seconds = (int) this.getTotalTime() / 1000;
 		int durationSeconds = seconds % 60;
 		int durationMinutes = (seconds / 60) % 60;
 		int durationHours = (seconds / 60) / 60;
-		
+
 		String durationString = String.format("%d:%02d:%02d", durationHours, durationMinutes, durationSeconds);
-		
+
 		return dateFormat.format(dateStart) + " - " + durationString;
-		}
-	
+	}
+
 	public List<Integer> getDuration() {
 		List<Integer> durations = new ArrayList<Integer>();
-		for (TimePair tp: this.getTimePairList()){
-			int durationSeconds = (int) tp.getElapsedTime() / 1000;
-			int durationMinutes = durationSeconds / 60;
-			int durationHours = durationMinutes / 60;
-			durations.add(durationHours);
-			durations.add(durationMinutes);
-			durations.add(durationSeconds);
-		}
+		int totalSeconds = (int) this.getTotalTime() / 1000;
+		int durationSeconds = totalSeconds % 60;
+		int durationMinutes = (totalSeconds / 60) % 60;
+		int durationHours = (totalSeconds / 60) / 60;
+		durations.add(durationHours);
+		durations.add(durationMinutes);
+		durations.add(durationSeconds);
 		return durations;
 	}
 }
