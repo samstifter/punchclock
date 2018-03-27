aee585570374a79116946102a3a950e9b752d3c5
Omair Ijaz <oijaz@iastate.edu>
2018-03-27 10:44:57 -0500

    Add javadoc and fix some formatting issues. Add an exception that checks if there is a negative TimePair

--- src/TimePair.java
+++ src/TimePair.java
@@ -5,53 +5,66 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * 
+ * This class defines a TimePair, which is a start time and an End Time
  *
  */
 public class TimePair {
 	private long start;
 	private long end;
-	
+
 	/**
 	 * Constructs a time pair with the given start and end times.
+	 * 
 	 * @param start
-	 * 			start time
+	 *            start time
 	 * @param end
-	 * 			end time
+	 *            end time
+	 * @throws IllegalArgumentException
+	 *             if the start time comes after the end time
 	 */
-	public TimePair(long start, long end) {
-		this.start = start;
-		this.end = end;
+	public TimePair(long start, long end) throws IllegalArgumentException {
+		if (start > end) {
+			throw new IllegalArgumentException();
+		} else {
+			this.start = start;
+			this.end = end;
+		}
 	}
-	
+
 	/**
-	 * Calculates the elapsed time of a time pair by
-	 * finding the difference between its end and start time
+	 * Calculates the elapsed time of a time pair by finding the difference
+	 * between its end and start time
 	 * 
 	 * @return Time between 2 times in milliseconds
 	 */
 	public long getElapsedTime() {
-        return end - start;
-    }
-	
+		return end - start;
+	}
+
 	/**
 	 * Gets the start time in milliseconds format
 	 * 
 	 * @return start time
 	 */
-	public long getStartTime(){
+	public long getStartTime() {
 		return this.start;
 	}
-	
+
 	/**
 	 * Gets the end time in milliseconds format
-	 *  
+	 * 
 	 * @return end time
 	 */
-	public long getEndTime(){
+	public long getEndTime() {
 		return this.end;
 	}
 
+	/**
+	 * Returns a list of the duration values for seconds, minutes, and hours.
+	 * List is formatted with hours first, minutes second, and seconds third
+	 * 
+	 * @return A list of the duration values
+	 */
 	public List<Integer> getDuration() {
 		int seconds = (int) this.getElapsedTime() / 1000;
 		int durationSeconds = seconds % 60;
@@ -63,18 +76,19 @@ public class TimePair {
 		durations.add(durationSeconds);
 		return durations;
 	}
-	
+
+	@Override
 	public String toString() {
-		
+
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
+
 		Date dateStart = new Date(this.start);
-		
+
 		// get duration and convert to seconds
 		List<Integer> durations = getDuration();
-		
+
 		String durationString = String.format("%d:%02d:%02d", durations.get(0), durations.get(1), durations.get(2));
-		
+
 		return dateFormat.format(dateStart) + " - " + durationString;
 	}
 }
