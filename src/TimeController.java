67bf9fd7d77ce50c4246f51120a03c5fc7d4c661
Omair Ijaz <oijaz@iastate.edu>
2018-03-27 10:51:17 -0500

    Refactor resetTime method. Add exception handling from TimePair. Refactor if the timer has started. Update javadoc.

--- src/TimeController.java
+++ src/TimeController.java
@@ -1,11 +1,6 @@
-import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  * The TimeController class is the Controller component of the MVC framework. It
@@ -18,10 +13,14 @@ import javafx.stage.FileChooser.ExtensionFilter;
 public class TimeController {
 	private TimeModel timeModel;
 	private TimeView timeView;
-	
+
 	/**
+	 * A controller for the PunchClock
+	 * 
 	 * @param timeModel
+	 *            model to be used
 	 * @param timeView
+	 *            view to be used
 	 */
 	public TimeController(TimeModel timeModel, TimeView timeView) {
 		this.timeModel = timeModel;
@@ -36,32 +35,38 @@ public class TimeController {
 	}
 
 	/**
+	 * Stop the timer
 	 * 
-	 * @return True if the time was started (aka time was started previously),
-	 *         false otherwise
+	 * @return True if the time was stopped as a result of this call, false otherwise
 	 */
 	public boolean stopTime() {
 		return timeModel.stopTime();
 	}
 
+	/**
+	 * Sets the name of the current session
+	 * 
+	 * @param sessionName
+	 *            Session name
+	 */
 	public void setSessionName(String sessionName) {
 		this.timeModel.setCurrentSessionName(sessionName);
 	}
 
 	/**
-	 * Resets the TimeModel, removing any TimePairs and "stopping" the time Also
-	 * writes the session to the file.
+	 * Ends the current session by stopping the time, adding the current session
+	 * to the list, and resetting time with a new session.
 	 */
-	public void resetTime() {
+	public void endCurrentSession() {
 		timeModel.stopTime();
+		timeModel.addSession(timeModel.getCurrentSession());
 		timeModel.resetTime();
-		this.saveSessions();
 	}
 
 	/**
-	 * Get the time of only the most recent session
+	 * Get the current session time in milliseconds
 	 * 
-	 * @return the elapsed time of the most recent session
+	 * @return Total elapsed milliseconds of current session
 	 */
 	public long getCurrentSessionElapsedTime() {
 		return timeModel.getCurrentSessionTime();
@@ -77,20 +82,42 @@ public class TimeController {
 		timeView.displayElapsedTimeInSeconds(timeModel);
 	}
 
+	/**
+	 * Edits the session stored in the session list at the specified index to
+	 * have the new duration by creating a new TimePair with the same start time
+	 * and an end time at the new duration after the start time
+	 * 
+	 * @param index
+	 *            Session index to edit
+	 * @param newDurationMillis
+	 *            new duration
+	 * @return true if edit was successful, false otherwise.
+	 */
 	public boolean editSession(int index, long newDurationMillis) {
 		try {
 			timeModel.getSessions().get(index).getTimePairList();
 			List<TimePair> list = new ArrayList<TimePair>();
 			long startTime = timeModel.getSessions().get(index).getTimePairList().get(0).getStartTime();
 			long endTime = startTime + newDurationMillis;
-			list.add(new TimePair(startTime, endTime));
-			timeModel.getSessions().get(index).setTimePairList(list);
+			try {
+				list.add(new TimePair(startTime, endTime));
+				timeModel.getSessions().get(index).setTimePairList(list);
+			} catch (IllegalArgumentException e) {
+				return false; // the end time is before the start time
+			}
 		} catch (IndexOutOfBoundsException e) {
 			return false;
 		}
 		return true;
 	}
 
+	/**
+	 * Delete a session at the index
+	 * 
+	 * @param index
+	 *            Index to delete
+	 * @return true if successful, false otherwise
+	 */
 	public boolean deleteSession(int index) {
 		try {
 			timeModel.getSessions().remove(index);
@@ -101,25 +128,35 @@ public class TimeController {
 	}
 
 	/**
-	 * Writes the time pairs of the current session to a file in CSV format.
+	 * Writes the sessions to a file in CSV format.
 	 * 
 	 * @return true if file is written, false otherwise.
 	 */
 	public boolean saveSessions() {
 		return timeModel.saveSessions();
 	}
-	
-	public boolean writeToReadableFile(LocalDate start, LocalDate end){
-		return timeModel.writeToReadableFile(start, end);
-	}
-	
+
 	/**
-	 * @return
+	 * Writes the sessions to a human readable format.
+	 * 
+	 * @return true if the write happens, false otherwise
 	 */
 	public boolean writeToReadableFile() {
 		return timeModel.writeToReadableFile();
 	}
-	
-	
+
+	/**
+	 * Writes the sessions from a specified date range to a human readable
+	 * format
+	 * 
+	 * @param start
+	 *            Start Date
+	 * @param end
+	 *            End Date
+	 * @return true if the file is written, false otherwise
+	 */
+	public boolean writeToReadableFile(LocalDate start, LocalDate end) {
+		return timeModel.writeToReadableFile(start, end);
+	}
 
 }
