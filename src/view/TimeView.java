2c32299414cd8802fe1d44ac9770c493c84f1c52
briang0 <brianguidarini@gmail.com>
2018-04-05 16:12:49 -0500

    Got rid of empty name tag

--- src/view/TimeView.java
+++ src/view/TimeView.java
@@ -273,8 +273,6 @@ public class TimeView extends Application {
 
 		Text applicationListTitle = new Text("Application to Track");
 		Text logNameTitle = new Text("Set Session Name");
-		
-		Text invalidName = new Text("(Empty name)");
 
 		ComboBox<String> applicationList = new ComboBox<String>();
 		applicationList.setValue("NONE");
@@ -282,8 +280,6 @@ public class TimeView extends Application {
 
 		TextField sessionName = new TextField();
 		sessionName.setPrefSize(150, 5);
-		
-		invalidName.setFill(Color.RED);
 		
 		Alert fileWriteAlert = new Alert(AlertType.ERROR, "File was unable to be written.");
 		fileWriteAlert.setHeaderText("File Save Error");
@@ -306,11 +302,6 @@ public class TimeView extends Application {
 			}
 			saveButton.setDisable(false);
 			clearButton.setDisable(false);
-			if (sessionName.getText().trim().length() != 0 && sessionName.getText() != null) {
-				invalidName.setText("");
-			}else {
-				invalidName.setText("(Empty name)");
-			}
 		});
 
 		saveButton.setOnAction(a -> {
@@ -341,14 +332,6 @@ public class TimeView extends Application {
 
 		sessionName.setOnKeyReleased(a -> {
 			timeController.setSessionName(sessionName.getText());
-			if (saveButtonReady && sessionName.getText().trim().length() != 0 && sessionName.getText() != null) {
-				invalidName.setText("");
-				
-			}else  if (sessionName.getText().trim().length() != 0 && sessionName.getText() != null){
-				invalidName.setText("");
-			}else{
-				invalidName.setText("(Empty name)");
-			}
 		});
 
 		applicationList.setOnShowing(a -> {
@@ -452,7 +435,7 @@ public class TimeView extends Application {
 		controlButtons.setAlignment(Pos.CENTER);
 
 		HBox logNames = new HBox(10);
-		logNames.getChildren().addAll(logNameTitle, sessionName, invalidName);
+		logNames.getChildren().addAll(logNameTitle, sessionName);
 		logNames.setAlignment(Pos.CENTER);
 
 		VBox applicationSelect = new VBox(3);
